458fa839d0a0902e8bf5bee151d674f4ad753744
Ricardo Silva <ricardo@salamandra.cc>
2017-06-12 13:43:19 -0300

    Updated version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django_mobile_auth',
-    version='0.2.1',
+    version='0.2.3',
     packages=find_packages(),
     license='MIT',
     author='Ricardo S. A. Silva',
